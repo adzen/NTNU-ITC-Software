b7b3a8b658f5e9e0dc87bd6ec0166e0d8652f46b
adzen <101adzen@gmail.com>
2013-01-25 01:00:45 +0800

    ListView can display contents in input file.

--- xml/xml/Form1.Designer.cs
+++ xml/xml/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.openFileButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
             this.SuspendLayout();
             // 
             // openFileButton
@@ -49,11 +50,22 @@
             this.openFileDialog1.Title = "開啟 info.xml 檔案";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
+            // listView1
+            // 
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(790, 357);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(814, 410);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.openFileButton);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -65,6 +77,7 @@
 
         private System.Windows.Forms.Button openFileButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListView listView1;
     }
 }
 
--- xml/xml/Form1.cs
+++ xml/xml/Form1.cs
@@ -6,12 +6,12 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Xml;
+
 namespace xml
 {
     public partial class Form1 : Form
-    {
-        public string xmlFileName = null;
-        
+    {       
         public Form1()
         {
             InitializeComponent();
@@ -24,7 +24,63 @@ namespace xml
 
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-            xmlFileName = openFileDialog1.FileName;
+            isTargetFile(openFileDialog1.FileName);
+        }
+
+        private bool isTargetFile(string filename)
+        {
+            XmlDocument doc = new XmlDocument();
+
+            try
+            {
+                doc.Load(filename);
+
+                XmlElement root = doc.DocumentElement;
+                if( !root.Name.Equals("table") ) return false;
+
+                XmlNode title_row = root.FirstChild;    // 標題行
+                if( !title_row.Name.Equals("row") ) return false;
+
+                XmlNodeList titles = title_row.ChildNodes;    // 標題的項目
+                if( titles.Count != 11 ) return false;
+                if( !titles.Item(0).Name.Equals("column") ) return false;
+
+                // 插入標題
+                listView1.Columns.Clear();
+                for (int i = 1; i < 11; ++i)
+                {
+                    listView1.Columns.Add(titles.Item(i).InnerText);
+                }
+
+                // 插入每一行
+                listView1.Items.Clear();
+                XmlNodeList entries = root.ChildNodes;    // 很多行
+                for (int i = 1; i < entries.Count; ++i)
+                {
+                    XmlNode one_row = entries.Item(i);    // 一行項目
+                    XmlNodeList row_entries = one_row.ChildNodes;    // 一行中的很多項目
+
+                    ListViewItem it = new ListViewItem();
+                    it.Text = row_entries.Item(1).InnerText.Trim();  // 流水號
+                    for (int j = 2; j < 11; ++j)
+                    {
+                        it.SubItems.Add(row_entries.Item(j).InnerText.Trim());   // 後面剩下的項目
+                    }
+                    listView1.Items.Add(it);   // 插入這行
+                }
+
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+
+
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "錯誤Orz", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            return true;
         }
     }
 }
